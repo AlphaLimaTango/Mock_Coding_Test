95ff483c0e9d895232721daf4f3d2266077d3751
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-05-01 15:34:52 +0100

    second test passing

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 var VendingMachine = function(items) {
   this.balance = 0;
-  this.nickel = 0;
-  this.dime = 0;
-  this.quarter = 0;
-  this.dollar = 0;
+  this.coins = [
+    { type: "nickle", value: 5, count: 0 },
+    { type: "dime", value: 10, count: 0 },
+    { type: "quarter", value: 25, count: 0 },
+    { type: "dollar", value: 100, count: 0 }
+  ];
   this.items = [
     { selector: "A", price: 65, count: 1 },
     { selector: "B", price: 100, count: 1 },
@@ -15,6 +17,22 @@ VendingMachine.prototype.getBalance = function() {
   return this.balance;
 };
 
+VendingMachine.prototype.receiveMoney = function(coinType) {
+  for (coin of this.coins) {
+    if (coin.type === coinType) {
+      this.balance += coin.value;
+    }
+  }
+};
+
+VendingMachine.prototype.sellItem = function(item) {
+  for (object of this.items) {
+    if (item === object.type) {
+      this.balance += this.items.selector;
+    }
+  }
+};
+
 let assert = require("assert");
 
 describe("Vending Machine", function() {
@@ -22,6 +40,14 @@ describe("Vending Machine", function() {
     it("Is zero when initially powered up", function() {
       machine = new VendingMachine();
       assert.strictEqual(0, machine.getBalance());
+    });
+  });
+
+  describe("Receive Money", function() {
+    it("Adds the value of coin to balance", function() {
+      machine = new VendingMachine();
+      machine.receiveMoney("nickle");
+      assert.strictEqual(5, machine.getBalance());
     });
   });
 });
